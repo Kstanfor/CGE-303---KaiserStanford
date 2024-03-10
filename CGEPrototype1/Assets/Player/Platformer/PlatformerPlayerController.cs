7095519ef165a73ee411b95d0c5b574fbfea5269
Kstanfor <158608252+Kstanfor@users.noreply.github.com>
2024-03-10 17:54:26 -0500

    jump

--- CGEPrototype1/Assets/Player/Platformer/PlatformerPlayerController.cs
+++ CGEPrototype1/Assets/Player/Platformer/PlatformerPlayerController.cs
@@ -5,29 +5,57 @@ using UnityEngine;
 public class PlatformerPlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+
+
     private Rigidbody2D rb;
+    private bool isGrounded;
     private float horizontalInput;
     // Start is called before the first frame update
 
     public AudioClip jumpSound;
     private AudioSource playerAudio;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
         playerAudio = GetComponent<AudioSource>();
+
+        if(groundCheck == null)
+        {
+            Debug.LogError("GroundCheck not assigned to the player controller!");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        //rb.velocity = new vector2();
-        //playerAudio.PlayOneShot(jumpSound, 1.0f);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            //playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
     }
 
     void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);  
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if(horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if(horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
     }
 }
